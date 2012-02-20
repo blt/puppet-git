09fe025b5bc19f95181e381d5de0ce01498c0ecd
Brian L. Troutwine <brian@troutwine.us>
2012-02-20 00:12:15 -0500

    Set owner and group of .gitconfig.
    I'm awfully bad at dropping owner and group off of file resources, mostly
    because I tend to create things _only_ for the root user. Anyway, I'm trying to
    reform.
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- manifests/resource/config.pp
+++ manifests/resource/config.pp
@@ -7,12 +7,15 @@
 #  [*ensure*]  - Set or destroy the .gitconfig file. (present|absent)
 #  [*root*]    - The root directory in which to place .gitconfig. Default /home/USER.
 #   *user*     - The user in question.
+#  [*group*]   - The user's primary group. Default USER.
 #  [*email*]   - The user's email address. Default USER@FQDN
 #   *realname* - The user's 'real' name. Default USER
 #
-define git::resource::config($ensure=file, $root="/home/$user", $user, $email="$user@$fqdn", $realname=$user) {
+define git::resource::config($ensure=file, $root="/home/$user", $user, $group=$user, $email="$user@$fqdn", $realname=$user) {
   file { "$root/.gitconfig":
     ensure => $ensure,
+    owner => $user,
+    group => $group,
     content => template('git/gitconfig.erb'),
     require => [File[$root], User[$user]],
   }
